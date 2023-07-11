9f2f2ec043d95d9119472232d6ee92ef089a178d
Odilbek <120348954+OdilbekIbrohimof@users.noreply.github.com>
2023-07-11 07:47:38 +0500

    add lesson 2

---
+++ lesson-2-loops/loops.py
@@ -0,0 +1,38 @@
+# import time
+# # while and for loops
+# start = time.time()
+# i = 0
+# while True:
+#     i += 1
+#     print(f"OK - {i}")
+#     if i > 10 ** 5:
+#         break
+# end = time.time()
+# print(f"Time is : {start - end}")
+
+
+# i = 0
+# while i < 100:
+#     i += 1
+#     print(i)
+
+# task  1
+# guess number oyinini yozing
+
+# task 2
+# while orqali kamida 5 ta odamni boyini sonlarda qabul qiling
+# ular orasida eng balandi eng past boyli odamdan necha sm baland ekanini
+# hisoblang
+
+# a = []
+# while True:
+#     i = int(input())
+#     a.append(i)
+#     if len(a) == 5:
+#         break
+# print(max(a) - min(a))
+
+# task 3
+# foydalanuvchidan toxtovsiz so'zlar qabul qiling agar sozlarda 2 tadan ortiq
+# a harfi ishlatilgan bo'lsa while ni toxtating
+
