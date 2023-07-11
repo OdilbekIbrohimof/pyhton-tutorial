9786cd472c2e1741df7c12abaabec41ae7f38829
Odilbek <120348954+OdilbekIbrohimof@users.noreply.github.com>
2023-07-11 07:51:26 +0500

    add

---
+++ lesson-3-loop-def/lesson/test.py
@@ -0,0 +1,119 @@
+# for i in range(10):
+#     print(i)
+
+# for x in [1,2,3,4]:
+#     print(x, end='')
+
+# print(range(10))
+
+# for i in range(0, 11, 2):
+#     print(i)
+
+# for k in range(0, 110, 10):
+#     print(k)
+
+# task 1
+# a = int(input("1-sonni kiriting: "))
+# b = int(input("2-sonni kiriting: "))
+# for i in range(a, b):
+#     print(i)
+
+# task 2
+# a = int(input("1-sonni kiriting: "))
+# b = int(input("2-sonni kiriting: "))
+# for i in range(a, b):
+#     c = (a + b) ** 2,
+#     print(i)
+#     print(c)
+
+# task 3
+
+
+# for i in range(10):print(i)
+
+# for i in range(30):
+#     n = 0
+#     if i == 15:
+#         break
+#     elif i == 10:
+#         continue
+# print(i)
+
+# def plus(x, y):
+#     return x + y
+
+
+# p = plus
+# print(p(6, 2))
+
+# task 1
+# def main(x, y):
+#     if x < y:
+#         print(y)
+
+#     else: print(x)
+
+# m = main
+# print(m(3, 8))
+
+# task 2
+# def sqre(x):
+#     return x ** 2
+
+# for i in range(1, 11):
+#     print(sqre(i))
+
+
+
+# def main(x):
+#     global a
+#     a = 10
+#     return x
+
+# print(main(1)) #1
+# print(a + 2) #12
+
+# def main(name=None, surname=None, position="worker"):
+#     return f"{name} {surname} {position}"
+
+# print(main())
+# print(main("john"))
+# print(main("john", "doe", "atsff"))
+# print(main("john", "doe"))
+
+# task 3
+# def main(name=None, surname=None, position="worker", age=None, sm=None):
+#     return f"{name} {surname} {position} {age} {sm}"
+
+# print(main())
+# print(main("john"))
+# print(main("john", "doe", "atsff"))
+# print(main("john", "doe"))
+# print(mian())
+# print(mian())
+
+
+# task 4
+# def num(num):
+#     return num 
+
+# for i in range(0, num):
+#     print(i)
+
+# n = num
+# print(n(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
