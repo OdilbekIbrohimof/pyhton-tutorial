6ac158ff500bac2e66a6c233edf230676fbf10e6
Odilbek <120348954+OdilbekIbrohimof@users.noreply.github.com>
2023-07-19 08:37:22 +0500

    lesson-7
    task

---
+++ Lesson-7-task/lesson/test.py
@@ -0,0 +1,18 @@
+# 10 ta 2 xonali 0 bilan tugasa yigindisini chiqarsin
+
+# task 1
+# t = input("soatni kiriting: ")
+# t.splite() 
+
+
+# task 2
+# num = int(input("10ta ikki xonali son kiriting: "))
+# for i in range(10):
+#     if num % 10:
+
+
+
+
+
+
+
