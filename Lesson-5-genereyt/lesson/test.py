a74e91b51ae44a639917b1b9d8e0e98bca0ba0a2
Odilbek <120348954+OdilbekIbrohimof@users.noreply.github.com>
2023-07-19 08:34:53 +0500

    add lesson 5

---
+++ Lesson-5-genereyt/lesson/test.py
@@ -0,0 +1,77 @@
+from random import random
+import random
+
+# print([x for i in range(20) for x in random.randint(150)])
+
+
+# a = list(range(10))
+# # a = random.sample(range(150, 200), 20)
+# print(a[1:5])
+
+# print(a[-1])
+
+# print("python"[2:])
+# print("python"[:2])
+# print("tomato"[:-1])
+
+# s = 'tomato'
+# a = s[:]
+# print(a)
+# print(list("python"))
+
+# a, b = "A", "B"
+# x, y, z = [1,2,3]
+# print(x, y, z)
+
+# l , *r = [1,1,1[]]
+
+# arr = [1,2,3]
+# arr2 = [4,5,6]
+# print(arr + arr2)
+# arr.extend(arr2)
+# print(arr)
+
+# l = list(range(3)) * 2
+# print("s" in "salom")
+# print(0 in l)
+# print(i not in l)
+# print(l)
+
+# l = list(range(5, 10))
+# for i, elem in enumerate(l):
+#     print(f"index = {i}")
+#     print(f"elem = {elem}")
+
+# arr = list(range(1, 11))
+# done_arr = []
+# for x in arr:
+#     done_arr.append(x ** 2)
+# print(done_arr)
+
+# print([x**2 for x in list(range(1,11))])
+
+# a = []
+# for i in range(1, 11):
+#     if i % 2 == 1:
+#         a.appendi
+# print(a)
+
+# print([i for i in range(1,11) if i % 2 == 1])
+
+# i = int(input("son kiriting: "))
+# status = "salom" if i > 5 else "xayr"
+# print(status)
+
+
+
+# task 1
+# a = random.sample(range(10, 35), 5)
+# print(a)
+
+# task 2
+# print([random.sample(range(10, 35), 5)])
+
+# task 3
+
+
+
