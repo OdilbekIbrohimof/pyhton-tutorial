f7a679b73c8226743cac2effbe6a826cd166293e
Odilbek <120348954+OdilbekIbrohimof@users.noreply.github.com>
2023-07-19 08:36:16 +0500

    add lesson-6-task

---
+++ Lesson-6-task/lesson/test.py
@@ -0,0 +1,60 @@
+# l = [x for x in range(5)]
+# print(l)
+
+# l = [x for x in range(5) if x > 2]
+# print(l)
+
+# arr = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+#     [10,11,12]
+# ]
+# done_arr_with_gen = [k for x in arr for k in x if k % 2 == 0]
+# print(done_arr_with_gen)
+
+# done_arr = []
+
+# for inner_arr in arr:
+#     for num in inner_arr:
+#         if num % 2 == 0:
+#             done_arr.append(num)
+
+# print(done_arr)
+
+
+
+# task 1
+
+
+# arr = [23,423,42,34,24,2,42,34,234,23,43,2,4,]
+# arr2 = [23,23,23,23,2,67,3,651,1,2,3,4,5,6,7,8,0]
+# count = []
+# for inner in arr:
+#     if inner not in count:
+#         if inner not in arr2:
+#             count.append(inner)
+
+# print(count)
+
+# arr = [23,423,42,34,24,2,42,34,234,23,43,2,4,]
+# count = []
+# for i in arr:
+#     if i not in count:
+#         count.append(i)
+
+# print(count)
+
+# task 2
+# chala
+
+
+
+# task 3
+
+
+
+
+
+
+
